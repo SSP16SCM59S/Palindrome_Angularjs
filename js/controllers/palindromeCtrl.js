cb98121d3021593201366955b9b740cb2cbac38a
shash5259 <sshanka5@hawk.iit.edu>
2017-06-01 08:59:16 -0500

    Landing Page with ParticleJs

---
+++ js/controllers/palindromeCtrl.js
@@ -0,0 +1,14 @@
+angular.module("PalindromeCtrlModule", [])
+
+.controller("palindromeCtrl",["$scope",function ($scope) {
+
+    document.getElementById("workspace").classList.remove('active');
+    document.getElementById("home").classList.add('active');
+
+    $scope.appDetails = {};
+    $scope.appDetails.title = "Palindromes";
+    $scope.appDetails.tagline = "Your One Stop for all Palindrome Problems!!";
+
+}])
+
+
